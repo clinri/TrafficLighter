ff00e2453ca4dee4b8408dd1b0d76b095be0b79b
Andrei Borisov <clinri@ya.ru>
2021-11-06 21:27:48 +0300

    Traffic lighter is done!

--- app/src/main/java/ru/clinri/trafficlighter/MainActivity.kt
+++ app/src/main/java/ru/clinri/trafficlighter/MainActivity.kt
@@ -1,11 +1,55 @@
 package ru.clinri.trafficlighter
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+    var imTrafLighter: ImageView? = null
+    var imageArray: IntArray = intArrayOf(
+        R.drawable.semafor_red,
+        R.drawable.semafor_yellow,
+        R.drawable.semafor_green
+    )
+    var counter: Int = 0
+    var timer: Timer? = null
+    var isRun: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imTrafLighter = findViewById(R.id.imTrafficLighter)
+
+    }
+
+    fun onClickStartStop(view: View) {
+        view as ImageButton
+        if (!isRun) {
+            startStop()
+            view.setImageResource(R.drawable.button_stop)
+            isRun = true
+        } else {
+            imTrafLighter?.setImageResource(R.drawable.semafor_grey)
+            view.setImageResource(R.drawable.button_start)
+            timer?.cancel() //останавливаем таймер если он не равен null
+            isRun = false
+            counter = 0
+        }
+    }
+
+    fun startStop() {
+        timer = Timer()
+        timer?.schedule(object : TimerTask() {
+            override fun run() {
+                runOnUiThread {
+                    imTrafLighter?.setImageResource(imageArray[counter])
+                    counter++
+                    if (counter == 3) counter = 0
+                }
+            }
+        }, 0, 1000)
     }
 }
